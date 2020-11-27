5b5dec1f0d784a63bf35a0c705457929bcbc1562
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2020-11-27 16:40:56 +0200

    Defining the dictionary array list

--- src/LZ78.java
+++ src/LZ78.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 
 public class LZ78 {
 	
 	private static String data = "";
+	private static ArrayList<String> dict = new ArrayList<String>();
 	
 	LZ78(String d){
 		data = d;
