30b02666915399e24984c3a2558ae512164a7928
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2020-11-27 19:28:51 +0200

    Finishing decompression algorithm

--- src/LZ78.java
+++ src/LZ78.java
@@ -84,8 +84,57 @@ public class LZ78 {
 		// print the dictionary
 		printDict();
 	}
+	
+	private void parsing(String inTags) {
+
+	     String insideTag = inTags.split("[\\\\<\\\\>]")[1];
+	     
+	     index = Integer.parseInt(insideTag.substring(0, insideTag.indexOf(",")));
+	     
+	     // check if next symbol is not null ' '
+	     if (insideTag.charAt(insideTag.indexOf("'") + 1) != '\'' )
+	    	 symbol = insideTag.charAt(insideTag.indexOf("'") + 1);
+	     else
+	    	 symbol = 0;
+	     
+	     tag = new Tag(index, symbol);
+	     allTags.add(tag);
+	     
+	     // if the previous wan't the last tag
+	     if(!inTags.equals("<" + insideTag + ">"))
+	    	 parsing(inTags.substring(insideTag.length() + 3)); // 3 = ' ' + '<' + '>'
+	}
 
 	public void decompress() {
+		
+		String original = "";
+		String restore = "";
+		
+		// load the input to the allTags array list
+		parsing(data);
+		
+		for(int i = 0; i < allTags.size(); i++) {
+			tag = allTags.get(i);
+			
+			// restore the data from the tag
+			restore = dict.get(tag.getIndex()) + tag.getSym();
+			
+			// append the restored chunk to the original
+			original += restore;
+			
+			// if the word restored is not in the dictionary
+			// and the last character restored is not null append to dictionary
+			if(!dict.contains(restore) && restore.charAt(restore.length() - 1) != ' ') {
+				
+				// add the new resulting chunk to dictionary for future references
+				dict.add(restore);
+			}
+		}
+
+		System.out.print("Original Data  : " + original);
+		
+		// print the dictionary
+		printDict();
 		
 	}
 }
